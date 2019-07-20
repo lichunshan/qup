57bac7b669dbf8afd1fe1a0ef9b40fd1df576c07
lichunshan <2375974889@qq.com>
2019-07-20 15:15:47 +0800

    调整删除文件逻辑文件结构

--- delete/index.js
+++ delete/index.js
@@ -2,7 +2,39 @@ var qiniu = require('qiniu');
 var path = require('path');
 var pkg = require('../package.json');
 var configFileName = pkg.name + ".config.js";
+function deleteFile(bucketManager, bucket, key){
+  bucketManager.delete(bucket, key, function(err, respBody, respInfo) {
+    if (err) {
+      console.log(err);
+      //throw err;
+    } else {
+      console.log(`${respInfo.statusCode} ${key} delete successful!`);
+    }
+  });
+}
 
+function getKeyLists(bucketManager, bucket, key, deleteFileCallBack){
+  var options = {
+    limit: 1000,
+    prefix: key + '/',
+  };
+  bucketManager.listPrefix(bucket, options, function(err, respBody, respInfo) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    if (respInfo.statusCode == 200) {
+      var items = respBody.items;
+      items.forEach(function(item) {
+        // console.log(item.key);
+        deleteFileCallBack(bucketManager, bucket, item.key);
+      });
+    } else {
+      console.log(respInfo.statusCode);
+      console.log(respBody);
+    }
+  });
+}
 module.exports = function(){
   var rootConf = require(path.join(process.cwd(),configFileName));
   var accessKey = rootConf.ak;
@@ -11,38 +43,6 @@ module.exports = function(){
   var config = new qiniu.conf.Config();
   config.zone = qiniu.zone["Zone_" + rootConf.zone];
   var bucketManager = new qiniu.rs.BucketManager(mac, config);
-  function deleteFile(bucket, key){
-    bucketManager.delete(bucket, key, function(err, respBody, respInfo) {
-      if (err) {
-        console.log(err);
-        //throw err;
-      } else {
-        console.log(`${respInfo.statusCode} ${key} delete successful!`);
-      }
-    });
-  }
-  function getKeyLists(bucket, key, deleteFileCallBack){
-    var options = {
-      limit: 1000,
-      prefix: key + '/',
-    };
-    bucketManager.listPrefix(bucket, options, function(err, respBody, respInfo) {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-      if (respInfo.statusCode == 200) {
-        var items = respBody.items;
-        items.forEach(function(item) {
-          console.log(item.key);
-          deleteFileCallBack(bucket, item.key);
-        });
-      } else {
-        console.log(respInfo.statusCode);
-        console.log(respBody);
-      }
-    });
-  }
-  getKeyLists(rootConf.options.scope, rootConf.key_prefix, deleteFile);
+  getKeyLists(bucketManager, rootConf.options.scope, rootConf.key_prefix, deleteFile);
 }
 
